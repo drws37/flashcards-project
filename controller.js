a0a28b1e8a92765787f1fa949af6d49fab2da2ae
alyonaosipova <alyonaosipova@gmail.com>
2023-12-15 11:53:31 +0300

    check

---
+++ controller.js
@@ -0,0 +1,25 @@
+const model = require("./model");
+const view = require("./view");
+
+class Controller {
+  constructor() {}
+}
+
+// const inquirer = require("inquirer");
+// inquirer
+//   .prompt([
+//     { type: "input", name: "username", message: "Введи имя:" },
+//     {
+//       type: "list",
+//       name: "bonuses",
+//       message: "На сколько ты опоздал?",
+//       choices: [
+//         { name: "Не опаздывал", value: 0 },
+//         { name: "На 2-3 минутки", value: 2 },
+//         { name: "Опоздал и не сообщил", value: 5 },
+//       ],
+//     },
+//   ])
+//   .then((answers) => console.log(answers));
+
+module.exports = Controller;
